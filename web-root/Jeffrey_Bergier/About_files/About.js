33c5ca05c2f5657598972fb50c4648dc68894828
Me <me@Lamps-Plus.local>
2025-09-20 19:11:45 +0900

    [iWeb] Added About Page

---
+++ web-root/Jeffrey_Bergier/About_files/About.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0 local-build-20250920
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.30,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_files/AboutMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ web-root/Jeffrey_Bergier/Blank_files/Blank.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0 local-build-20250920
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Blank_files/BlankMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
